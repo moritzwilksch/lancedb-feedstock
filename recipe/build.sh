af1f6b072dcc78201c7e8611d86f50b000e2d624
Moritz Wilksch <moritzwilksch@gmail.com>
2024-03-31 16:04:21 +0000

    make build work

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,12 +7,9 @@ cargo-bundle-licenses \
   --format yaml \
   --output ${SRC_DIR}/THIRDPARTY.yml
 
-# Apply PEP517 to install the package
+maturin build --manifest-path python/Cargo.toml --release -i $PYTHON
 
-cd python/
-maturin build --release -i $PYTHON
-
-cd ../target/wheels
+cd target/wheels
 
 # Install wheel manually
 $PYTHON -m pip install *.whl
