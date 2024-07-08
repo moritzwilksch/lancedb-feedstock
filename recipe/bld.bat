be77324055ad30ec26d89db882de9985c8be8efb
Uwe L. Korn <uwe.korn@quantco.com>
2024-07-08 15:40:19 +0200

    win: cd into python

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,4 +7,6 @@ REM maturin build --manifest-path python\Cargo.toml --release -i %PYTHON%
 REM cd target\wheels
 REM %PYTHON% -m pip install *.whl
 
-%PYTHON% -m pip install --no-deps --ignore-installed python
+pushd python
+%PYTHON% -m pip install --no-deps --ignore-installed .
+popd
